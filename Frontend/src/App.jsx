db4d4d6f5c3d199e0f62b8813d0eb4ba44b447f5
Rishabh_1098 <119887536+Rishabh-1098git@users.noreply.github.com>
2024-08-23 13:46:08 +0530

    Initial commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+  return <div className="font-mono text-3xl bg-slate-400">HeathCare SIH</div>;
+}
+
+export default App;
---
+++ README.md
@@ -0,0 +1,2 @@
+# HealthCare
+ SIH Project -  Open Innovation
